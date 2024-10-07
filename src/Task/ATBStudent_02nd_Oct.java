6dc3b684f692200de97260bcbb54772d90e45a8d
91957 <reshmamali76@gmail.com>
2024-10-07 19:22:31 +0530

    Add 2nd Oct task

---
+++ src/Task/ATBStudent_02nd_Oct.java
@@ -0,0 +1,21 @@
+package Task;
+
+public class ATBStudent_02nd_Oct {
+    String Stud_name;
+    String field;
+    String clg_name;
+    char grade;
+    boolean fees;
+
+
+    void teach(){
+
+    }
+    void speech(){
+
+    }
+
+    void learn(){
+
+    }
+}
---
+++ src/Task/Main_ATBStudent_02nd_Oct.java
@@ -0,0 +1,36 @@
+package Task;
+
+public class Main_ATBStudent_02nd_Oct {
+    public static void main(String[] args) {
+        ATBStudent_02nd_Oct stud1 = new ATBStudent_02nd_Oct();
+        stud1.Stud_name = "Sharad";
+        stud1.field = "Art";
+        stud1.grade= 'B';
+        System.out.println(stud1.Stud_name);
+        System.out.println(stud1.field);
+        System.out.println(stud1.grade);
+
+
+        ATBStudent_02nd_Oct stud2 = new ATBStudent_02nd_Oct();
+        stud2.Stud_name = "Reshma";
+        stud2.field = "Science";
+        stud2.grade= 'A';
+        System.out.println(stud2.Stud_name);
+        System.out.println(stud2.field);
+        System.out.println(stud2.grade);
+
+        ATBStudent_02nd_Oct stud3 = new ATBStudent_02nd_Oct();
+        stud3.Stud_name = "Reeva";
+        stud3.field = "Commerce";
+        stud3.grade= 'A';
+        System.out.println(stud3.Stud_name);
+        System.out.println(stud3.field);
+        System.out.println(stud3.grade);
+
+        ATBStudent_02nd_Oct stud4 = new ATBStudent_02nd_Oct();
+
+        new ATBStudent_02nd_Oct();
+
+
+    }
+}
